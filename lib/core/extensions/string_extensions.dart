6eaa1c273ef32731558cbf4f7bc7cd09653e29ad
unknown <formatable2018@gmail.com>
2024-04-09 10:24:02 +0100

    [setup] fvm for project

--- lib/core/extensions/string_extensions.dart
+++ lib/core/extensions/string_extensions.dart
@@ -1,16 +1,18 @@
 import 'package:qoute_app/constants/app_constants.dart';
 
 extension StringX on String {
-  // an extension to get a validated email on string
+  /// an extension to get a validated email on string
   bool get isValidEmail => Constants.emailRegex.hasMatch(this);
-  // an extension to get a validated name on string
+
+  /// an extension to get a validated name on string
   bool get isValidFullName => Constants.fullNameRegex.hasMatch(this);
-  // an extension to get a validated contact on string
+
+  /// an extension to get a validated contact on string
   bool get isValidContact => Constants.contactRegex.hasMatch(this);
 
-  // get app images
+  /// get app images
   String toImage() => "assets/images/$this.png";
 
-  // get app svgs
+  /// get app svgs
   String toSvg() => "assets/svgs/$this.svg";
 }
